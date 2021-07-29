3c8cdffd1b046316572b1641d97feeaeeccd8ba7
Salvador <55848411+Salvador243@users.noreply.github.com>
2021-07-28 21:00:50 -0500

    the models had been added

---
+++ app/DetailsProfile.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DetailsProfile extends Model
+{
+    //
+}
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,11 +1,11 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '95deb6eb8a9ba084a177d0b874c902fd224dd0a5',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -383,12 +383,12 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '95deb6eb8a9ba084a177d0b874c902fd224dd0a5',
             'dev_requirement' => false,
         ),
         'laravel/socialite' => array(
