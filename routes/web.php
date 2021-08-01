05fe1aa5129437052ed19c90f5a252cedc140aba
Kevin A <kevin.arias13m@gmail.com>
2021-08-01 18:42:57 -0500

    I added Ajax in search bar to make asynchronous queries.

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('index');
+Route::get('/search', 'HomeController@search')->name('search');
+
 
 //Google Login
 Route::group(['prefix' => 'auth'], function () {
