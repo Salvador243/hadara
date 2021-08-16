261acb780a81c48fc793304d65b754b15961a1f5
Kevin A <kevinarias13m@gmail.com>
2021-08-15 21:49:56 -0500

    Login width had been changed.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,67 +3,63 @@
 @section('content')
 
 
-<div class="container pleca col-md-3 my-5 rounded-new padding-pleca">
-	<div class="row justify-content-md-center">
-		<img src="{{asset('/storage/logo.svg')}}" weight="170" height="170"alt="">
-	</div>
-	<div class="container">
-		<form method="POST" action="{{ route('login') }}">
-			@csrf
-			<div class="d-flex justify-content-center my-4">
-				<input id="email" type="email" class="rounded-login @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-mail">
+<div class="container pleca col-md-4 my-5 rounded-new padding-pleca">
+    <div class="row justify-content-md-center">
+        <img src="{{asset('/storage/logo.svg')}}" weight="150" height="150" alt="">
+    </div>
+    <div class="container">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="my-4">
+                <input id="email" type="email" class="rounded-login @error('email') is-invalid @enderror" name="email"
+                    value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-mail">
 
-				@error('email')
-				<span class="invalid-feedback" role="alert">
-					<strong>{{ $message }}</strong>
-				</span>
-				@enderror
-			</div>
+                @error('email')
+                <span class="invalid-feedback d-block mt-1" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
 
-			<div class="d-flex justify-content-center mt-4">
-				<input id="password" type="password" class="rounded-login @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
+            <div class="mt-4">
+                <input id="password" type="password" class="rounded-login @error('password') is-invalid @enderror"
+                    name="password" required autocomplete="current-password" placeholder="Password">
 
-				@error('password')
-				<span class="invalid-feedback" role="alert">
-					<strong>{{ $message }}</strong>
-				</span>
-				@enderror
-			</div>
+                @error('password')
+                <span class="invalid-feedback d-block mt-1" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
 
-			<div class="d-flex justify-content-end">
-				@if (Route::has('password.request'))
-				<a class="btn-log pt-2" href="{{ route('password.request') }}">
-					{{ __('Forgot Your Password?') }}
-				</a>
-				@endif
-			</div>
+            <div class="d-flex justify-content-end">
+                @if (Route::has('password.request'))
+                <a class="btn-log pt-2" href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+                @endif
+            </div>
 
-			<div class="d-flex justify-content-center pt-5 pb-4">
-				<button type="submit" class="btn btn-danger">
-					{{ __('Login') }}
-				</button>	
-			</div>
+            <div class="d-flex justify-content-center pt-5 pb-4">
+                <button type="submit" class="btn btn-danger">
+                    {{ __('Login') }}
+                </button>
+            </div>
 
-			<div class="d-flex justify-content-center">
-				<h2 class="line-div description"><span class="text-pleca">Or</span></h2>
-			</div>
+            <div class="d-flex justify-content-center">
+                <h2 class="line-div description"><span class="text-pleca">Or</span></h2>
+            </div>
 
-			<div class="d-flex justify-content-center pt-4">
-				<p class="text-secondary nav-text">Sign Up Using</p>
-			</div>
+            <div class="d-flex justify-content-center pt-4">
+                <p class="text-secondary nav-text">Sign Up Using</p>
+            </div>
 
-			<div class="d-flex justify-content-center">
-				<a href="{{ url('/auth/google') }}" class="" role="button">
-					<img src="https://img.icons8.com/fluency/48/000000/google-logo.png"/>
-				</a>
-			</div>
-		<div class="col-auto">
-			<div class="d-flex justify-content-center">
-				<h2 class="line-div description"><span class="text-pleca">Or</span></h2>
-			</div>
-		</div>
-		</form>
-	</div>
+            <div class="d-flex justify-content-center">
+                <a href="{{ url('/auth/google') }}" class="" role="button">
+                    <img src="https://img.icons8.com/fluency/48/000000/google-logo.png" />
+                </a>
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
-
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,49 +3,48 @@
 @section('content')
 
 
-<div class="container pleca col-md-3 my-5 rounded padding-pleca">
+<div class="container pleca col-md-4 my-5 rounded padding-pleca">
     <div class="row justify-content-md-center">
-        <img src="{{asset('/storage/logo.svg')}}" weight="170" height="170"alt="">
+        <img src="{{asset('/storage/logo.svg')}}" weight="150" height="150"alt="">
     </div>
     <div class="container">
      <form method="POST" action="{{ route('register') }}">
         @csrf
-        <div class="d-flex justify-content-center my-4">
+        <div class="my-4">
             <input id="name" type="text" class="rounded-login @error('name') is-invalid @enderror"
             name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Name">
-
             @error('name')
-            <span class="invalid-feedback" role="alert">
+            <span class="invalid-feedback d-block mt-1" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
         </div>
 
-        <div class="d-flex justify-content-center my-4">
+        <div class="my-4">
             <input id="email" type="email" class="rounded-login @error('email') is-invalid @enderror"
             name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="E-mail">
 
             @error('email')
-            <span class="invalid-feedback" role="alert">
+            <span class="invalid-feedback d-block mt-1" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
         </div>
 
-        <div class="d-flex justify-content-end my-4">
+        <div class="my-4">
 
             <input id="password" type="password"
             class="rounded-login @error('password') is-invalid @enderror" name="password"
             required autocomplete="new-password" placeholder="password">
 
             @error('password')
-            <span class="invalid-feedback" role="alert">
+            <span class="invalid-feedback d-block mt-1" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
         </div>
 
-        <div class="d-flex justify-content-center pb-5">
+        <div class="pb-5">
             <input id="password-confirm" type="password" class="rounded-login"
             name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Password">   
         </div>
