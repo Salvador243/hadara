bb3251547bc12261f8e18d83e3e77f3db244e5c9
Kevin A <kevinarias13m@gmail.com>
2021-08-16 21:37:27 -0500

    Show upload image preview in edit profile.

--- resources/views/upload_images.blade.php
+++ resources/views/upload_images.blade.php
@@ -5,56 +5,51 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.0/dropzone.js"></script>
 {{--container principal--}}
 <div class="container mt-4">
-	<form method="post" action="{{ route('save') }}" 
-	enctype="multipart/form-data">
-	@csrf
-	<div class="pleca rounded-new">
-		{{-- 2 Columns pleca --}}
-		<div class="row col-sm-auto justify-content-center padding-pleca">
-			{{-- first column --}}
-			<div class="col-sm-6">
-				<div class="container">
-					<div class="col-sm-12 my-5 px-5">
-						<input id="fichero" type="file" name="img_name">
-						<label for="fichero" class="circle">Seleccionar Archivo</label>
-					</div>
-				</div>
-			</div>
-			{{-- second column --}}
-			<div class="col-sm-5">
-				<div class="input-group">
-					<input type="text" class="rounded form-control my-4" name="title" 
-					placeholder="Inserte Titulo">
-				</div>
-				<div class="input-group">
-					<input type="text" class="rounded form-control my-4" name="description" 
-					placeholder="Inserta Descripcion">
-				</div>
-				<div class="input-group">
-					<input type="text" class="rounded form-control my-4" name="tags" 
-					placeholder="Etiquetas" disabled="">
-				</div>
-				<div class="d-grid gap-2 d-md-flex justify-content-md-end">
-					<button class="my-3 btn btn-danger" type="">publicar</button>
-				</div>
-			</div>
-		</div>
-	</div>
-</form>
-</div>
+    <form method="post" action="{{ route('save') }}" enctype="multipart/form-data">
+        @csrf
+        <div class="pleca rounded-new">
+            {{-- 2 Columns pleca --}}
+            <div class="row col-sm-auto justify-content-center padding-pleca">
+                {{-- first column --}}
+                <div class="col-sm-6">
+                    <div class="container">
+                        <div class="col-sm-12 my-5 px-5">
+                            <input id="fichero" type="file" name="img_name">
+                            <label for="fichero" class="circle" id="preview">Seleccionar Archivo</label>
+                        </div>
+                    </div>
+                </div>
+                {{-- second column --}}
+                <div class="col-sm-5">
+                    <div class="input-group">
+                        <input type="text" class="rounded form-control my-4" name="title" placeholder="Inserte Titulo">
+                    </div>
+                    <div class="input-group">
+                        <input type="text" class="rounded form-control my-4" name="description"
+                            placeholder="Inserta Descripcion">
+                    </div>
+                    <div class="input-group">
+                        <input type="text" class="rounded form-control my-4" name="tags" placeholder="Etiquetas"
+                            disabled="">
+                    </div>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button class="my-3 btn btn-danger" type="">publicar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
 </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-@endsection	
+<script>
+	//Set upload image preview
+	$(document).ready(function () {
+		//When the user upload a picture...
+		$("#fichero").change(function (ev) {
+			var url = URL.createObjectURL(ev.target.files[0]);
+			$("#preview").attr('style', 'background-image: url(' + url + '); background-size: cover;');
+		});
+	});
+</script>
+@endsection
