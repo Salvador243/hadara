b3e43f799dcb551d437d01adce56e01649d67002
Salvador <55848411+Salvador243@users.noreply.github.com>
2021-11-26 18:52:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
 </head>
 
-<body style="background-color: #000;">
+<body>
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
